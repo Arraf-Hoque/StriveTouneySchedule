7b5b9dae6741240d97e0d7fd41de825768f8824c
Arraf-Hoque <101687776+Arraf-Hoque@users.noreply.github.com>
2023-08-02 11:20:01 -0600

    Updating the main.py stuff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # This example requires the 'message_content' intent.
-
 import discord
 from discord.ext import commands
 
@@ -9,13 +8,16 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+            
     async def on_message(self, message):
         if message.author == self.user:
             return
         if message.content == "?schedule":
-            await message.channel.send("I'm working on it!")
-     
-    
+            scheduleEmbed = discord.Embed (title = "Saturday Tournament Times", colour = None) # create embed
+            scheduleEmbed.add_field(name = "9Moons strive", value = "Start Time: <t:1690923600:t> \n [start.gg link](https://start.gg/9moonsGGST)")
+            scheduleEmbed.add_field(name = "CptnHawkeyes Beginner Beatdown", value = "Start Time: <t:1690930800:t> \n [start.gg link](https://start.gg/beginner_beatdown)", inline=False)
+            await message.channel.send(embed=scheduleEmbed)
+
 
 
 intents = discord.Intents.default()
