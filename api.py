7a95e860af8c8390098be742cb8e233031facc95
Arraf-Hoque <101687776+Arraf-Hoque@users.noreply.github.com>
2023-08-07 10:28:19 -0600

    Update api.py

--- api.py
+++ api.py
@@ -10,7 +10,8 @@ print("Rows: ", wks.row_count)
 print("Columns, ", wks.col_count)
 
 
-search = "NA"
+search = "NA" # filter value
 
-values = [r for r in wks.get_all_values() if r[5] == search]
-print(values, "\n")
+values = [r for r in wks.get_all_values() if r[5] == search] # get all the rows where column 5 (region) contains NA itll store the rows in a list
+
+print(values) # print everything in the list
